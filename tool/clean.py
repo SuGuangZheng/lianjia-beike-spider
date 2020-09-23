e66247358ba80a626ad277b80a41412107b6fedd
SuGuangzheng <466164829@qq.com>
2020-09-23 14:20:27 +0800

    基础本地适配修改

--- tool/clean.py
+++ tool/clean.py
@@ -8,16 +8,16 @@ from lib.utility.path import *
 
 if __name__ == '__main__':
     # 删除日志
-    os.system("rm -rf {0}/*.txt".format(LOG_PATH))
+    os.system("rd /s /q {0}/*.txt".format(LOG_PATH))
 
     # 删除爬取的数据
-    os.system("rm -rf {0}/*".format(DATA_PATH))
+    os.system("rd /s /q {0}/*".format(DATA_PATH))
 
     # 删除HTML
-    os.system("rm -rf {0}/*.html".format(ROOT_PATH))
+    os.system("rd /s /q {0}/*.html".format(ROOT_PATH))
 
     # 删除csv
-    os.system("rm -rf {0}/*.csv".format(ROOT_PATH))
+    os.system("rd /s /q {0}/*.csv".format(ROOT_PATH))
 
     # 删除json
-    os.system("rm -rf {0}/*.json".format(ROOT_PATH))
+    os.system("rd /s /q {0}/*.json".format(ROOT_PATH))
